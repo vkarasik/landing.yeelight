54c5778dac922ad6e72e3225cdf1cb5b39e68845
Vadim Karasik <vadkarasik@gmail.com>
2019-11-19 15:04:16 +0300

    add products

---
+++ app/yilai-snow/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Cache-control" content="no-cache">
+		<meta http-equiv="Pragma" content="no-cache">
+        <meta http-equiv="Expires" content="-1">
+
+        <title>Yeelight | Потолочный светильник Yilai Snow Ceiling Light</title>
+        <meta name="description" content="Светодиодный Потолочный светильник Yilai Snow Ceiling Light"/>
+        <?php include '../includes/head.html'; ?>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <?php 
+                include '../includes/header.html'; 
+                include '../includes/content_yilai-snow.html';
+                include '../includes/footer.html'; 
+            ?>
+        </div>
+        <?php include '../includes/counts.html'; ?>
+    </body>
+</html>
