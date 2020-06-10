80729c2f2f5bbfb62045cd79fbe7a2de279b0c1b
Vadim Karasik <vadkarasik@gmail.com>
2020-06-10 16:25:11 +0300

    add new page

---
+++ app/wireless-charge-nightlight/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Cache-control" content="no-cache">
+		<meta http-equiv="Pragma" content="no-cache">
+        <meta http-equiv="Expires" content="-1">
+
+        <title>Yeelight | Подвесной Crystal Pendant Light</title>
+        <meta name="description" content="Светодиодный подвесной светильник Crystal Pendant Light"/>
+        <?php include '../includes/head.html';?>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <?php
+include '../includes/header.html';
+include '../includes/content_wireless-charge-nightlight.html';
+include '../includes/footer.html';
+?>
+        </div>
+        <?php include '../includes/counts.html';?>
+    </body>
+</html>
