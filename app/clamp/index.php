1f73e0464c071ae88bce7911a6444713a79e2d92
Vadim Karasik <vadkarasik@gmail.com>
2020-06-16 14:29:57 +0300

    add new page

---
+++ app/clamp/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Cache-control" content="no-cache">
+		<meta http-equiv="Pragma" content="no-cache">
+        <meta http-equiv="Expires" content="-1">
+
+        <title>Yeelight | Rechargeable Desk Clamp Lamp J1 — настольная лампа</title>
+        <meta name="description" content="Yeelight Rechargeable Desk Clamp Lamp J1 — настольная лампа"/>
+        <?php include '../includes/head.html';?>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <?php
+include '../includes/header.html';
+include '../includes/content_clamp.html';
+include '../includes/footer.html';
+?>
+        </div>
+        <?php include '../includes/counts.html';?>
+    </body>
+</html>
