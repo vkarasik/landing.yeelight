4f53aec2dca91fdf93ae9645938c559c94f55bfe
Vadim Karasik <vadkarasik@gmail.com>
2021-05-25 16:50:42 +0300

    add desktop page Yeelight Curtain

---
+++ app/curtain-motor/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta http-equiv="Cache-control" content="no-cache">
+		<meta http-equiv="Pragma" content="no-cache">
+        <meta http-equiv="Expires" content="-1">
+
+        <title>Yeelight | Yeelight Smart Electric Curtain Motor & Stitching Track — умный карниз</title>
+        <meta name="description" content="Yeelight Smart Electric Curtain Motor & Stitching Track — умный карниз"/>
+        <?php include '../includes/head.html';?>
+    </head>
+    <body>
+        <div class="container-fluid">
+            <?php
+include '../includes/header.html';
+include '../includes/content_curtain.html';
+include '../includes/footer.html';
+?>
+        </div>
+        <?php include '../includes/counts.html';?>
+    </body>
+</html>
